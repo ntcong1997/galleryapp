8def2790514e18fbdb00976800664d6dd68db9bd
cong <ntcong1997@gmail.com>
2023-04-16 23:35:38 +0700

    feat: implement permission denied in videos screen

--- feature/videos/src/main/java/com/fossil/galleryapp/feature/videos/VideosScreen.kt
+++ feature/videos/src/main/java/com/fossil/galleryapp/feature/videos/VideosScreen.kt
@@ -1,6 +1,10 @@
 package com.fossil.galleryapp.feature.videos
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +19,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.fossil.galleryapp.core.model.Media
+import com.fossil.galleryapp.core.ui.PermissionSettingsDialog
 import com.fossil.galleryapp.core.ui.mediaCardItems
 import com.fossil.galleryapp.core.ui.permissionItem
 
@@ -26,13 +31,22 @@ import com.fossil.galleryapp.core.ui.permissionItem
 fun VideosRoute(
     viewModel: VideosViewModel = hiltViewModel()
 ) {
+    val activity = LocalContext.current as? Activity
+    val context = LocalContext.current
+
     var permissionGranted by remember { mutableStateOf(false) }
+    var showPermissionSettingsDialog by remember { mutableStateOf(false) }
     val videos = viewModel.videos.collectAsLazyPagingItems()
 
     val requestGalleryPermission =
         rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
             if (it.containsValue(false)) {
                 permissionGranted = false
+                if (activity?.shouldShowRequestPermissionRationale(
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) android.Manifest.permission.READ_MEDIA_VIDEO
+                        else android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == false
+                ) showPermissionSettingsDialog = true
                 return@rememberLauncherForActivityResult
             }
             permissionGranted = true
@@ -41,6 +55,7 @@ fun VideosRoute(
 
     VideosScreen(
         permissionGranted = permissionGranted,
+        showPermissionSettingsDialog = showPermissionSettingsDialog,
         videos = videos,
         onAllowClick = {
             requestGalleryPermission.launch(
@@ -49,6 +64,17 @@ fun VideosRoute(
                     else android.Manifest.permission.READ_EXTERNAL_STORAGE
                 )
             )
+        },
+        onSettingsClick = {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", context.packageName, null)
+            intent.data = uri
+            context.startActivity(intent)
+
+            showPermissionSettingsDialog = false
+        },
+        onDismissPermissionSettingsDialog = {
+            showPermissionSettingsDialog = false
         }
     )
 
@@ -65,8 +91,11 @@ fun VideosRoute(
 @Composable
 fun VideosScreen(
     permissionGranted: Boolean,
+    showPermissionSettingsDialog: Boolean,
     videos: LazyPagingItems<Media>,
-    onAllowClick: () -> Unit
+    onAllowClick: () -> Unit,
+    onSettingsClick: () -> Unit,
+    onDismissPermissionSettingsDialog: () -> Unit
 ) {
     val context = LocalContext.current
 
@@ -75,6 +104,13 @@ fun VideosScreen(
         else permissionItem(
             content = context.getString(R.string.content_videos_permission),
             onAllowClick = onAllowClick
+        )
+    }
+
+    if (showPermissionSettingsDialog) {
+        PermissionSettingsDialog(
+            onSettingsClick = onSettingsClick,
+            onDismissRequest = onDismissPermissionSettingsDialog
         )
     }
 }
